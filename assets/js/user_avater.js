92af7b28879fffffe3b14d557b8d3f0496cfe637
PraPlus <1692922905@qq.com>
2022-05-05 11:14:24 +0800

    完成个人中心功能的开发

---
+++ assets/js/user_avater.js
@@ -0,0 +1,62 @@
+$(function () {
+    // 1.1 获取裁剪区域的 DOM 元素
+    var $image = $('#image')
+    // 1.2 配置选项
+    const options = {
+        // 纵横比
+        aspectRatio: 1,
+        // 指定预览区域
+        preview: '.img-preview'
+    }
+
+    // 1.3 创建裁剪区域
+    $image.cropper(options)
+
+    // 模拟点击上传
+    $('#btnChooseImg').on('click', function (e) {
+        // e.preventDefault()
+
+        $('#file').click()
+    })
+    $('#file').on('change', function (e) {
+        var filelist = e.target.files
+        if (filelist === 0) {
+            return layui.layer.msg('请选择照片')
+        }
+        // 拿到用户选择的文件
+        var file = e.target.files[0]
+        // 将文件转化为路径
+        var imgURl = URL.createObjectURL(file)
+        // 重新初始化裁剪区域
+        $image
+            .cropper('destroy')      // 销毁旧的裁剪区域
+            .attr('src', imgURl)  // 重新设置图片路径
+            .cropper(options)        // 重新初始化裁剪区域
+    })
+
+    // 为确定按钮绑定点击事件
+    $('#btnUpload').on('click', function () {
+        // 拿到用户裁剪之后的图片
+        var dataURL = $image
+            .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+                width: 100,
+                height: 100
+            })
+            .toDataURL('image/png')       // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+        // 调用接口把头像上传到服务器
+        $.ajax({
+            method: "POST",
+            url: "/my/update/avatar",
+            data: {
+                avatar: dataURL
+            },
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg('上传图片失败')
+                }
+                layui.layer.msg('上传图片成功')
+                window.parent.getUserInfo()
+            }
+        })
+    })
+})
